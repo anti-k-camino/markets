a4049b31f9686b730f9ec984bab86e450106c196
antonoff <kratyuk.anton84@gmail.com>
2018-11-17 19:54:06 +0200

    initial commit

---
+++ start.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config({ path: 'variables.env' });
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) =>  console.error(`Error  → ${err.message}`);
+
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Running → PORT ${server.address().port}`);
+});
